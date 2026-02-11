18b42b68ed222e76fbbffa62e5481cd4f18f4f97
iammiracle <miracleaayodele@gmail.com>
2026-02-11 12:21:58 +0100

    improved pre hook, incase another module has defined website_description

--- sale_quotation_builder/hooks.py
+++ sale_quotation_builder/hooks.py
@@ -1,34 +1,28 @@
 def pre_init_hook(env):
     """Allow installing sale_quotation_builder in databases
     with large sale.order / sale.order.line tables.
-
     Since website_description fields computation is based
     on new fields added by the module, they will be empty anyway.
-
     By avoiding the computation of those fields,
     we reduce the installation time noticeably
     """
-    env.cr.execute(
-        """
-        ALTER TABLE "sale_order"
-        ADD COLUMN "website_description" text
-    """
-    )
-    env.cr.execute(
-        """
-        ALTER TABLE "sale_order_line"
-        ADD COLUMN "website_description" text
-    """
-    )
-    env.cr.execute(
-        """
-        ALTER TABLE "sale_order_template_line"
-        ADD COLUMN "website_description" text
-    """
-    )
-    env.cr.execute(
-        """
-        ALTER TABLE "sale_order_template_option"
-        ADD COLUMN "website_description" text
-    """
-    )
+
+    def add_column_if_not_exists(table_name, column_name, column_type='text'):
+        env.cr.execute("""
+                       SELECT column_name
+                       FROM information_schema.columns
+                       WHERE table_name = %s
+                         AND column_name = %s
+                       """, (table_name, column_name))
+
+        if not env.cr.fetchone():
+            env.cr.execute(f"""
+                ALTER TABLE "{table_name}"
+                ADD COLUMN "{column_name}" {column_type}
+            """)
+
+    # Add website_description columns to all required tables
+    add_column_if_not_exists('sale_order', 'website_description')
+    add_column_if_not_exists('sale_order_line', 'website_description')
+    add_column_if_not_exists('sale_order_template_line', 'website_description')
+    add_column_if_not_exists('sale_order_template_option', 'website_description')
